698488d93cee3b72da43817e172b9f4affa36735
-fsociety <sangeet40404@gmail.com>
2026-04-12 16:53:27 +0530

    Time: 1 ms (99.9%), Space: 45.6 MB (37.87%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int idx =0 ;
+        for(int i = 0 ;i<n;i++){
+            char ch = chars[i];
+            int count = 0;
+            while(i < n && chars[i]==ch){
+                count ++;i++;
+            }
+            if(count ==1){
+                chars[idx++] = ch;
+            }
+            else{
+                chars[idx++] = ch;
+                String str = Integer.toString(count);
+                for(char digit : str.toCharArray() ){
+                    chars[idx++] = digit;
+                }
+            }
+            i--;
+
+        }
+        return idx;
+    }
+}
