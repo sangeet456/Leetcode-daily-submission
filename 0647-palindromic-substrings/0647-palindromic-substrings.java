4c1c03503e836b0af46ede91261a467fa10233d9
-fsociety <sangeet40404@gmail.com>
2026-04-12 15:44:40 +0530

    Time: 911 ms (5.02%), Space: 43.1 MB (47.13%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -0,0 +1,19 @@
+class Solution {
+    private static boolean check( String s , int  i , int j ){
+        if(i>j) return true;
+        if(s.charAt(i) == s.charAt(j)){
+            return check(s , i+1 , j-1);
+        
+}
+return false;    }
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count =0;
+       for(int i =0 ;i<n;i++){
+        for(int j=i;j<n;j++){
+            if(check(s,i,j)) count ++;
+        }
+       }
+        return count;
+    }
+}
