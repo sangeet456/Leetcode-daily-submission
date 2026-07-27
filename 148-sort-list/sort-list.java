a227147a2a5ed431618ac21e75b51931212b8eb8
-fsociety <sangeet40404@gmail.com>
2026-07-27 09:36:33 -0400

    Time: 12 ms (32.26%) | Memory: 64 MB (11.72%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,28 @@
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+    
+        ListNode temp = head;
+        int count =0;
+        while(temp!=null){
+            count ++;
+            temp = temp.next;
+        }
+        int arr[] = new int[count];
+        temp = head;
+        int idx=0;
+        while(temp!=null){
+            arr[idx]=temp.val;
+            idx ++;
+            temp=temp.next;
+        }
+        Arrays.sort(arr);
+        temp=head;
+        idx =0;
+        while(temp!=null){
+            temp.val = arr[idx];
+            idx ++;
+            temp = temp.next;
+        }
+   return head; }
+}
