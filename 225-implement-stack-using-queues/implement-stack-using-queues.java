b2779e4d4beddded4d19214f0b0cd5cd4aacdb18
-fsociety <sangeet40404@gmail.com>
2026-07-28 12:39:46 -0400

    Time: 0 ms (100.00%) | Memory: 42.8 MB (49.35%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,23 @@
+class MyStack {
+    Stack<Integer> st = new Stack<>();
+    
+    public MyStack() {
+        //constructor 
+    }
+    
+    public void push(int x) {
+        st.push(x);
+    }
+    
+    public int pop() {
+        return st.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public boolean empty() {
+        return st.isEmpty();
+    }
+}
