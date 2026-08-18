ee64c0b82827a561f1a8a70a19da8971767a9da2
-fsociety <sangeet40404@gmail.com>
2026-08-18 15:53:08 +0530

    Time: 7 ms (16.54%) | Memory: 45.6 MB (13.24%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -1,3 +1,9 @@
+//euclid algo can be use for this also//
+// gcd(a,b) --- gcd (a-b,b) if a>b //
+//gcd (a,b) --------->>> gcd(a,b-a) if b>a //
+
+
+
 class Solution {
 private static int gcd(int x , int y){
        if(x==0) return y;
